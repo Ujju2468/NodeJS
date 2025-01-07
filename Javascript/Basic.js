1056960cedb7481062dbfc6aa4164a4170b9854a
Ujjawal patel <97520638+Ujju2468@users.noreply.github.com>
2025-01-07 17:00:31 +0000

    day1

---
+++ Javascript/Basic.js
@@ -0,0 +1,20 @@
+//4 types to get output in js
+// FIRST
+console.log('Hello');
+// SECOND
+// document.write('Hello')
+// THIRD
+// document.getElementById("Demo").innerHTML="Hello"
+// FOURTH
+window.alert('Hello')
+
+
+//Javascript is written between <script></script>
+{/* <script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script> */}
+//Javascript runs line to line and also has block scope 
+
+//let, var, const are the variables.
+//Number,string,boolean,object
+
